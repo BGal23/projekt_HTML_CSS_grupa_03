cac54c52237d79e174da881ee24d101477b435e9
AdrianZ96 <adrianzdziarski96@wp.pl>
2023-07-08 21:50:41 +0200

    kilka drobnych zmian
    up

--- src/js/modal-products.js
+++ src/js/modal-products.js
@@ -19,4 +19,30 @@
     }
 
     // Dodanie nasłuchiwacza dla zamykania modala
-    closeBtn.addEventListener("click", closeModal);
+    closeBtn.addEventListener("click", closeModal); 
+    
+    // Pobierz elementy formularza
+  const form = document.getElementById('myForm');
+  const nameInput = document.getElementById('nameInput');
+  const phoneInput = document.getElementById('phoneInput');
+  const submitButton = document.getElementById('submitButton');
+
+  // Utwórz funkcję sprawdzającą, czy pola są wypełnione
+  function checkFields() {
+    const nameValue = nameInput.value.trim();
+    const phoneValue = phoneInput.value.trim();
+
+    // Sprawdź, czy pola name i phone mają odpowiednie wartości
+    if (nameValue !== '' && phoneValue !== '') {
+      submitButton.disabled = false; // Odblokuj przycisk
+    } else {
+      submitButton.disabled = true; // Zablokuj przycisk
+    }
+  }
+
+  // Nasłuchuj zmian w polach tekstowych
+  nameInput.addEventListener('input', checkFields);
+  phoneInput.addEventListener('input', checkFields);
+
+  // Zablokuj przycisk na początku
+  checkFields();

dfc98bc18fd30df57f993797f9d34ffd1b8c4b51
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-07-08 08:56:01 +0200

    fix js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './sass/main.scss';
-import './js/modal';
-import './js/modal-header';
+//import './js/modal';
+//import './js/modal-header';
 import './js/modal-products.js';
 import './js/modal-mobile-menu';
 import './js/modal-buy-now';

928240eb9e5f7d354e0436ddf81ed238331761cb
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-07-09 19:49:36 +0200

    1.0

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import './sass/main.scss';
-//import './js/modal';
-//import './js/modal-header';
 import './js/modal-products.js';
 import './js/modal-mobile-menu';
 import './js/modal-buy-now';
 import './js/modal-our-locations';
 import './js/modal-franchise2';
+import './js/modal-read-more';

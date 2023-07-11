0d44e113dfd54a7047293895ea26929556b2f6d7
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-07-11 19:01:18 +0000

    Deploying to gh-pages from @ BGal23/projekt_HTML_CSS_grupa_03@40b9699426fe4d8c8aaafa6e2d34337e2320eaee 🚀

--- index.d6432eb2.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-pagination",prevEl:".swiper-pagination"}});
-//# sourceMappingURL=index.d6432eb2.js.map
